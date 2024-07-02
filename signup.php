8d08014ff7f2acc48fa3974a759502ae7831737b
SriRanjani30 <Sriranjani935@gmail.com>
2024-07-02 09:53:50 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,5 +1,5 @@
 <?php
-// Connect to the database
+// Replace 'your_password' with the actual root password
 $conn = new mysqli('localhost', 'root', 'root@123', 'hackclub');
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
@@ -11,7 +11,7 @@ $email = $_POST['email'];
 $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
 
 // Insert the user data into the database
-$sql = "INSERT INTO users (Username, Email, Password) VALUES ('$username', '$email', '$password')";
+$sql = "INSERT INTO signup (Username, Email, Password) VALUES ('$username', '$email', '$password')";
 if ($conn->query($sql) === TRUE) {
     echo "Signup successful!";
 } else {
